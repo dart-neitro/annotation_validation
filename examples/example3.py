cc43bc822dca186d07a2ff9b82827c9cff8c8e2c
neitro <admin@rnd2.ru>
2019-03-18 22:19:41 +0300

    added examples

---
+++ examples/example3.py
@@ -0,0 +1,31 @@
+"""
+Custom class
+"""
+from annotation_validation import check_types, AnnotationTypeError
+
+
+class MyClass:
+    def __init__(self, n):
+        self.n = n
+
+    def __eq__(self, other):
+        return self.n == other.n
+
+
+@check_types
+def func(a: MyClass):
+    return a
+
+
+try:
+    z = list()
+    z.append(func(MyClass(1)))
+    z.append(func(MyClass(2)))
+    func('c')
+except AnnotationTypeError:
+    pass
+except Exception as e:
+    raise e
+
+
+assert z == [MyClass(1), MyClass(2)]
